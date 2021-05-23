64c93f086422e3aecc9ba142e31aaba53c976d55
jessica you <jessicaxytan@gmail.com>
2021-05-23 13:56:01 -0400

    commit tree 05178e34a862f77396d35b0f27e42af4f7db2aba

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(data => console.log(data))
+})
